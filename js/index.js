56198bbe5a424fe0220ece71faca5989d669b256
innakondur <innakondur@gmail.com>
2019-09-09 15:41:22 +0000

    sample

---
+++ js/index.js
@@ -0,0 +1,30 @@
+document.querySelector('.marker')
+  .innerText = (new Date()).toLocaleTimeString();
+
+  document.querySelector('.get-ajax-html')
+    .addEventListener('click', getHtmlAjax);
+
+function getHtmlAjax() {
+    const XHR_STATE_FINISHED = 4;
+    const HTTP_STATUS_OK = 200;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XHR_STATE_FINISHED
+            && xhr.status === HTTP_STATUS_OK) {
+               document.querySelector('.html-container')
+                 .innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'client-data.html', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-html')
+    .addEventListener('click', fetchHtml);
+
+function fetchHtml() {
+    fetch('client-data.html')
+        .then( response => response.text() )
+        .then( html => document.querySelector('.html-container')
+                 .innerHTML = html );
+}
